572f7c56d3849a43d2d264ed14d70d2dbb42697c
connor <cleu309fw@gmail.com>
2022-06-29 18:05:42 -0500

    gonna rewrite again eventually this shit is broken yee yeegit add *

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,2 @@
-pub mod lex;
-pub mod token;
-pub mod solver;
-
 fn main() {
-    let mut solver = solver::Solver::new_expr("-12491.3");
-    solver.as_reverse_polish();
-    solver.debug_dump();
-    // test.debug_dump();
 }
--- src/token.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum Token {
-    Operation(char, u8, bool), // operation (as character), precedence, is left associative
-    Number(f32),
-    LParen,
-    RParen,
-}
